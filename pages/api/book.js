ed0068b7b3c603f911cbc1e2d692b947583c956d
daniellanorris <daniellanorris@ufl.edu>
2023-07-21 19:37:09 -0600

    all tests pass, can't get books to add to favorite

--- pages/api/book.js
+++ pages/api/book.js
@@ -1,63 +1,51 @@
 import { withIronSessionApiRoute } from "iron-session/next";
-import { userInfo } from "os";
-import { runInNewContext } from "vm";
 import sessionOptions from "../../config/session"
 import db from '../../db'
-import user from "../../db/models/user";
 
 // this handler runs for /api/book with any request method (GET, POST, etc)
 export default withIronSessionApiRoute(
 
   async function handler(req, res) {
-    const user = req.session.user;
-    const props = {}
-    if(user) {
-      props.user = req.session.user;
-      props.isLoggedIn = true
-    } else {
-      props.isLoggedIn = false
-    }
-
-    if(props.isLoggedIn) {
-      res.status(200)
-    } else {
-      req.session.destroy()
-      res.status(401).end()
-    } 
-
-    console.log(props.user.id)
-
+      if (!req.session.user) {
+        return res.status(401).json({ error: "User Not Found" })
+      }
+  
     // TODO: On a POST request, add a book using db.book.add with request body (must use JSON.parse)
     // TODO: On a DELETE request, remove a book using db.book.remove with request body (must use JSON.parse)
-    const bodyParsed = req.body ? JSON.parse(req.body) : null
       switch(req.method) {
         case 'POST' :
-          if(!bodyParsed) {
-            return res.status(400).json({error: error.message})
-          }
           try  {
-          await db.book.add(props.user.id, bodyParsed);
-          return res.status(200).json({message: 'book added', method: 'POST'})
-          } catch (error) {
+          const bodyParsed = JSON.parse(req.body) 
+          const bookAdd = await db.book.add(req.session.user.id, bodyParsed);
+
+          if(bookAdd === null) {
+            req.session.destroy()
+            return res.status(401).json({error: "User not found"})
+          }
+
+          return res.status(200).end()
+          }
+           catch (error) {
             return res.status(400).json({error: error.message})
           }
+
+
           case 'DELETE':
-            if (!bodyParsed) {
-              return res.status(400).json({ error: error.message});
-            }
             try {
+              const bodyParsed = JSON.parse(req.body) 
               const bookDelete = await db.book.remove(req.session.user.id, bodyParsed.id);
               if (bookDelete === null) {
-                await req.session.destroy()
-                res.status(401).end()
-              } if(bookDelete !== null) {
-              return res.status(200).json({ message: 'Book deleted', method: 'DELETE' });
-              }
+                req.session.destroy()
+                return res.status(401).json({error: "User Not Found"})
+              } 
+              return res.status(200).json(bodyParsed);
+            
             } catch (error) {
               return res.status(400).json({ error: error.message });
             }
+
             default: 
-            return res.status(401).end()
+            return res.status(404).end()
         
       }
     // TODO: Respond with 404 for all other requests
--- pages/book/[id].jsx
+++ pages/book/[id].jsx
@@ -49,19 +49,22 @@ export default function Book(props) {
     // TODO: use fetch to call POST /api/book
     // Be sure to pass book in body (use JSON.stringify)
     // Call router.replace(router.asPath) if you receive a 200 status
-    e.preventDefault()
-    const res = await fetch('/api/book' ,  {
-      method: 'POST', 
+
+    const favorites = []
+
+    const res = await fetch('/api/book/', {
+      method: 'POST',
       headers: {
-        "content-type": "application/json",
-      }, 
-      body: JSON.stringify(props.book)
-    })
+        'content-type': 'application/json', 
+      },
+      body: JSON.stringify(book),
+    });
+    
+    favorites.push(book)
+
     if (res.status == 200) {
       router.replace(router.asPath)
-
     }
-    
   }
   async function removeFromFavorites(e) {
     // TODO: use fetch to call DELETE /api/book
